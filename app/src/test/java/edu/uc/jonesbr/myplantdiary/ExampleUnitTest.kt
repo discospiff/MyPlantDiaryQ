66083614a1153a6505cfdaf26f28d8c8135532d2
jonesbr <brandan.jones@uc.edu>
2020-01-26 15:41:08 -0500

    Add a DTO unit test.

--- app/src/test/java/edu/uc/jonesbr/myplantdiary/ExampleUnitTest.kt
+++ app/src/test/java/edu/uc/jonesbr/myplantdiary/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package edu.uc.jonesbr.myplantdiary
 
+import edu.uc.jonesbr.myplantdiary.dto.Plant
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -36,8 +37,9 @@ class ExampleUnitTest {
         assertEquals(8, 4 + 4)
     }
 
+    @Test
     fun confirmEasternRedbud_outputsEasternRedbud () {
-        var plant:Plant = Plant("Cercis", "canadesis", "Eastern Redbud")
+        var plant: Plant = Plant("Cercis", "canadesis", "Eastern Redbud")
         assertEquals("Eastern Redbud", plant.toString());
     }
 
