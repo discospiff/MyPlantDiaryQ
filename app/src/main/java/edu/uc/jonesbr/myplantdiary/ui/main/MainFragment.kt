3d2e64125e58f34d92ddcf80a410955e9f3d782d
jonesbr <brandan.jones@uc.edu>
2020-02-12 20:26:31 -0500

    Take photo and show thumbnail using implicit intent.

--- app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainFragment.kt
@@ -1,17 +1,28 @@
 package edu.uc.jonesbr.myplantdiary.ui.main
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import edu.uc.jonesbr.myplantdiary.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
+
+    private val CAMERA_REQUEST_CODE: Int = 1998
+    val CAMERA_PERMISSION_REQUEST_CODE = 1997
 
     companion object {
         fun newInstance() = MainFragment()
@@ -32,7 +43,60 @@ class MainFragment : Fragment() {
         viewModel.plants.observe(this, Observer {
             plants -> actPlantName.setAdapter(ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, plants))
         })
+        btnTakePhoto.setOnClickListener {
+            prepTakePhoto()
+        }
 
+    }
+
+    /**
+     * See if we have permission or not.
+     */
+    private fun prepTakePhoto() {
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePhoto()
+        } else {
+            val permissionRequest = arrayOf(Manifest.permission.CAMERA);
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted, let's do stuff.
+                    takePhoto()
+                } else {
+                    Toast.makeText(context, "Unable to take photo without permission", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun takePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+            takePictureIntent -> takePictureIntent.resolveActivity(context!!.packageManager)?.also {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+        }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE)  {
+                // now we can get the thumbnail
+                val imageBitmap = data!!.extras!!.get("data") as Bitmap
+                imgPlant.setImageBitmap(imageBitmap)
+            }
+        }
     }
 
 }
